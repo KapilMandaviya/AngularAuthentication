bc03dae6140d9d0a051f56e0257cac9acd168a60
KapilMandaviya <mandaviyakapil34179@gmail.com>
2025-01-15 17:40:40 +0530

    commit changes 55555

--- WebApiProject/APIDEMOCORE/Employee.cs
+++ WebApiProject/APIDEMOCORE/Employee.cs
@@ -21,6 +21,7 @@ namespace APIDEMOCORE
         public string ResetPasswordToken { get; set; }
         public DateTime? ResetPassExpiry { get; set; }
         public DateTime? ResetPassExpiry22222 { get; set; }
-        
+        public DateTime? ResetPassExpiry666666 { get; set; }
+
     }
 }
