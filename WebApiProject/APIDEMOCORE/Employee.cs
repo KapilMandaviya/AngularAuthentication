c5fed11c16c6d45b1ff38cb5b19f367ffa02a3de
KapilMandaviya <mandaviyakapil34179@gmail.com>
2025-01-15 16:54:55 +0530

    test2 branch employee changes

--- WebApiProject/APIDEMOCORE/Employee.cs
+++ WebApiProject/APIDEMOCORE/Employee.cs
@@ -20,5 +20,7 @@ namespace APIDEMOCORE
         public DateTime? ExpiryTime { get; set; }
         public string ResetPasswordToken { get; set; }
         public DateTime? ResetPassExpiry { get; set; }
+
+        public string ResetPasswordTokentest_2 { get; set; }
     }
 }
